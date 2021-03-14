643e950e4c1d32ab77d184a6c40b6adb18acb94d
roki.hisui <roki.hisui@gmail.com>
2021-03-14 20:37:44 +0900

    docを追加

--- dto/user_dto.py
+++ dto/user_dto.py
@@ -2,9 +2,16 @@
 class User:
 
     def __init__(self, id, name, rank):
+        """
+        コンストラクタ
+        ID、ユーザ名、ランクを設定
+        """
         self.id = id
         self.name = name
         self.rank = rank
 
     def __str__(self):
+        """
+        内部で保持している情報の文字列表現
+        """
         return "id:{}, name:{}, rank:{}".format(self.id, self.name, self.rank)
